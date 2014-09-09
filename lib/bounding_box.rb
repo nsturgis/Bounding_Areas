89d449572b2099d1837077d52360f5b2ef1a53d2
Helen Hood <helen.c.hood@gmail.com>
2014-09-09 11:22:26 -0400

    Fix indentation and whitespace

--- lib/bounding_box.rb
+++ lib/bounding_box.rb
@@ -1,10 +1,11 @@
 class BoundingBox
   attr_reader :left, :bottom, :width, :height
+
   def initialize (left, bottom, width , height)
-   @left = left
-   @bottom = bottom
-   @height = height
-   @width = width
+    @left = left
+    @bottom = bottom
+    @height = height
+    @width = width
   end
 
   def right
@@ -16,10 +17,10 @@ class BoundingBox
   end
 
   def contains_point?(x, y)
-  if x >= left && x <= right && y >= bottom && y <= top
-    return true
-  else
-    return false
-  end
+    if x >= left && x <= right && y >= bottom && y <= top
+      return true
+    else
+      return false
+    end
   end
 end
