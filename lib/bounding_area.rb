ede4551711bc9ce13d6aa9a72af8e3f597d159df
Helen Hood <helen.c.hood@gmail.com>
2014-09-09 11:23:59 -0400

    Rename @box instance variable to be more descriptive & add attr_reader

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,14 +1,16 @@
 class BoundingArea
+  attr_reader :boxes
+
   def initialize(box_array)
-    @box = box_array
+    @boxes = box_array
   end
 
   def contains_point?(x, y)
     if x == 0 && y == 0
       return false
     end
-    @box.each do |row|
-      return row.contains_point?(x, y)
+    boxes.each do |box|
+      return box.contains_point?(x, y)
     end
   end
 end
